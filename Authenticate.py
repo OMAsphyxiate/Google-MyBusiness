ba15e26b3fd6710333ecb3174a03de21596ba0bb
OMAsphyxiate <omasphyxiate@gmail.com>
2018-03-29 08:21:14 -0500

    Update Authenticate.py

--- Authenticate.py
+++ Authenticate.py
@@ -2,8 +2,8 @@ import requests
 from requests_oauthlib import OAuth2Session
 
 
-client_id = r'697833433089-g2kk8gir32of7f0brvrdi6ekpkqkubau.apps.googleusercontent.com' #Google MB API client ID
-client_secret = r'MymK8Pxuy6pog_fij0Rb6dji' #Google MB secrete
+client_id = r'ClientID' #Google MB API client ID
+client_secret = r'ClientSecret' #Google MB secrete
 redirect_url = 'http://ddsview.com/GMB/MyBusiness' #Redirect URL
 redirect_local = 'localhost:8080'
 scope_url = ['https://www.googleapis.com/auth/plus.business.manage', '']
@@ -18,6 +18,6 @@ authorization_response = raw_input('Enter the full callback URL')
 
 token = oauth.fetch_token('https://accounts.google.com/o/oauth2/token', authorization_response=authorization_response, client_secret=client_secret)
 
-r = oauth.get('https://mybusiness.googleapis.com/v3/accounts/107604422420814997819/locations') #Submit get request using token just fetched
+r = oauth.get('https://mybusiness.googleapis.com/v3/accounts/AccountID/locations') #Submit get request using token just fetched
 print(r.text)
 #Request URL: https://mybusiness.googleapis.com/v3/accounts
